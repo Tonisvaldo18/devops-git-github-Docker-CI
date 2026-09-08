b718ea932348c11f6f43780b1d6ef75bddfae0bb
Tonisvaldo18 <tonisvaldo2014@gmail.com>
2026-09-08 16:21:44 +0100

    create dockerized api

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.path.append("app")
+
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json["message"] == "Hello DevOps!"
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json["status"] == "ok"
